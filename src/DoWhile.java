02977939a227e80d7abac14cfa2b65f2de52b7c0
OlehBoholiub <boholiub95@gmail.com>
2021-09-26 06:48:10 -0600

    DoWhileLoop

---
+++ src/DoWhile.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+       // System.out.println("Input 5!");
+        //int value = scanner.nextInt();
+       // while(value!=5){
+          //  System.out.println("Input 5!");
+           // value = scanner.nextInt();
+        //}
+        int value;
+        do{
+            System.out.println("Input 5!");
+            value = scanner.nextInt();
+        } while (value!=5);
+        System.out.println("You did");
+    }
+}
