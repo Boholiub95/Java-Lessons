441f6684195ee52b7472f473332e6ae6477b5f98
OlehBoholiub <boholiub95@gmail.com>
2021-09-25 21:50:22 -0600

    ForLoop

---
+++ src/ForLoop.java
@@ -0,0 +1,6 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int i = 0; i<=10;  i++){              // i++ == i+1 (true)
+            System.out.println("Hello"+i);
+        }
+    }}
