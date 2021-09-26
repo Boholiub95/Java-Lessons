4593007fa472366786d22234a9e2fe989d2ff6e8
OlehBoholiub <boholiub95@gmail.com>
2021-09-26 06:29:53 -0600

    Input

---
+++ src/Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+       // String s = new String("adlg");  // java.lang
+        Scanner s = new Scanner(System.in); // java.util
+        System.out.println("Input something");
+        ///String string = s.nextLine();
+        int x = s.nextInt();
+        System.out.println("you did");
+    }
+}
