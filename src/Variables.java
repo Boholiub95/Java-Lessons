defc336b0720e31b801251ca03ee676c5f3be0e9
OlehBoholiub <boholiub95@gmail.com>
2021-09-25 18:16:52 -0600

    Variables

---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String [] args){
+        int myInt = 557;
+        short myShort = 3266;
+        long myLong = 250000;
+        double myDouble = 255.33;
+        float myFloat = 2366.f;
+        char c ='a';
+        boolean b = true; //false
+        byte myByte = 100; // -128 to 128b
+
+        System.out.println(myInt);
+
+    }
+}
