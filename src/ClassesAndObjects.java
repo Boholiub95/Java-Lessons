3e3e4809ff4fe43f3f7c5a6ee763fb7f8093067c
OlehBoholiub <boholiub95@gmail.com>
2021-09-27 16:07:04 -0600

    Method

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -3,19 +3,28 @@ public class ClassesAndObjects {
         Person  person1 = new Person();
         person1.name = "Oleh";
         person1.age = 50;
-        System.out.println("My name is "+person1.name+","+"I am"+"."+person1.age+"years old");
+        person1.speak();
+        //System.out.println("My name is "+person1.name+","+"I am"+"."+person1.age+"years old");
         Person person2 = new Person();
         person2.name = "Vova";
         person2.age = 20;
-        System.out.println("My name is "+person2.name+","+"I am"+","+person2.age+"years old");
+        person2.speak();
+        ///System.out.println("My name is "+person2.name+","+"I am"+","+person2.age+"years old");
 
     }
 }
+
 class Person{
     // Class can have
     //  1 Date
     // 2 Actions
     String name;
     int age;
+    void speak(){
+        for(int i =0;i<3;i++) {
+            System.out.println("My name " + name + "," + "I am" + age + "," + "years old");
+        }
+
+    }
 
 }
