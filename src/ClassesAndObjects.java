b36f509b37d8d7b5a5ab20a5ab526d9bc1aafc01
OlehBoholiub <boholiub95@gmail.com>
2021-09-29 20:48:07 -0600

    ClassesAndObject 3.0

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,13 +1,15 @@
 public class ClassesAndObjects {
     public static void main(String[] args) {
         Person  person1 = new Person();
-        person1.name = "Oleh";
+        //person1.name = "Oleh";
+        person1.setNameAndAge("Oleh",20);
+        String s1 = "Vova";
         person1.age = 50;
         //person1.speak();
         //System.out.println("My name is "+person1.name+","+"I am"+"."+person1.age+"years old");
         Person person2 = new Person();
-        person2.name = "Vova";
-        person2.age = 20;
+        person2.setNameAndAge(s1,30);
+        //person2.age = 20;
         //person2.speak();
         person1.calculateYearsToRetirement();
         person2.calculateYearsToRetirement();
@@ -15,6 +17,8 @@ public class ClassesAndObjects {
         int year2 = person2.calculateYearsToRetirement1();
         System.out.println(year1);
         System.out.println(year2);
+        person1.speak();
+        person2.speak();
         ///System.out.println("My name is "+person2.name+","+"I am"+","+person2.age+"years old");
 
     }
@@ -26,6 +30,11 @@ class Person{
     // 2 Actions
     String name;
     int age;
+    void setNameAndAge(String username, int userage){
+        name = username;
+        age = userage;
+
+    }
     void  calculateYearsToRetirement(){
         int years = 65-age;
         System.out.println("Retirement"+ years);
