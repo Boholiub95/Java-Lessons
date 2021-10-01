f1d6018328a7e51094542d30701fc05ed62b3ef1
OlehBoholiub <boholiub95@gmail.com>
2021-09-30 21:09:38 -0600

    Lessons 20

--- src/Lesson19.java
+++ src/Lesson19.java
@@ -1,33 +1,31 @@
 public class Lesson19 {
     public static void main(String[] args) {
-        Human human1 = new Human();
-        human1.setAge(18);
-        human1.setName("Oleh");
-        human1.getInfo();
-        Human human2 = new Human();
-        human2.setAge(19);
-        human2.setName("Bob");
-        human2.getInfo();
+       Human human1 = new Human();
+
 
     }
 
 }
 class  Human{
-    String name;
-    int age;
-    public void setName(String name){
-        this.name = name;
+
+
+    private String name;
+    private int  age;
+
+
+    public Human(){
+     this.name = "Name";
+     this.age = 0;
     }
-    public  void  setAge(int age){
-        this.age =  age;
-    }
-    public String getName(){
-        return  name;
-    }
-    public  int getAge(){
-        return age;
-    }
-    public void getInfo(){
-        System.out.println(name+","+age);
-    }
+    public  Human(String name){
+       this.name = name;
+     }
+     public Human(String name,int age){
+         System.out.println("Hello3");
+         this.name = name;
+         this.age = age;
+
+     }
+    public void setName(String name){this.name = name;}
+    public  void  setAge(int age){this.age =  age;}
 }
