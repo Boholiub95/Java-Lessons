d17d5652a113d89c16ef5a2183c2987de51b797d
OlehBoholiub <boholiub95@gmail.com>
2021-10-02 20:24:23 -0600

    final

---
+++ src/Lesson22.java
@@ -0,0 +1,17 @@
+public class Lesson22 {
+    public static void main(String[] args) {
+        /*Test.CONSTANT = 10;
+        */
+
+    }
+}
+class Test{
+    public static final int CONSTANT = 0;
+   /* public Test (int CONSTANT){
+        this.CONSTANT = CONSTANT;
+    }
+    /*public void setCONSTANT(int x){
+        this.CONSTANT = x;
+    }
+     */
+}
