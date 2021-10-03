e9eaf62bc0b0259f8fbb2d8dd0b7a09effc20c6a
OlehBoholiub <boholiub95@gmail.com>
2021-10-02 20:50:15 -0600

    stringBuilder

---
+++ src/Lesson23.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+
+public class Lesson23 {
+    public static void main(String[] args) {
+     /*    String x = "hello";
+         x = x.toUpperCase();
+        System.out.println(x);*/
+        /*String string1 = "Hello";
+        String string2 = " my";
+        String string3 = " friend";
+        String stringAll = string1+string2+string3;
+        System.out.println(stringAll);*/
+        StringBuffer sb = new StringBuffer("Hello");
+        System.out.println(sb.toString());
+        sb.append(" my");
+        sb.append(" friends");
+        System.out.println(sb.toString());
+
+    }
+}
