ebc5a52ef7137469697bcdde9dfb1d3a9cccae39
OlehBoholiub <boholiub95@gmail.com>
2021-10-02 21:05:35 -0600

    Sb

--- src/Lesson23.java
+++ src/Lesson23.java
@@ -10,11 +10,13 @@ public class Lesson23 {
         String string3 = " friend";
         String stringAll = string1+string2+string3;
         System.out.println(stringAll);*/
-        StringBuffer sb = new StringBuffer("Hello");
+        /*StringBuffer sb = new StringBuffer("Hello");
         System.out.println(sb.toString());
         sb.append(" my");
         sb.append(" friends");
-        System.out.println(sb.toString());
+        System.out.println(sb.toString());*/
+        System.out.printf("This is a string %s","Nice");
+        System.out.println("String %10d \n", 45.3);
 
     }
 }
