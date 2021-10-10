6aae5eaee1572720da0dae42433c609d772971b8
OlehBoholiub <boholiub95@gmail.com>
2021-10-10 16:02:47 -0600

    Used object Account

---
+++ src/UseAccount.java
@@ -0,0 +1,33 @@
+import static java.lang.System.out;
+public class UseAccount {
+    public static void main(String[] args) {
+        Account myAccount;
+        Account yourAccount;
+        myAccount = new Account ();
+        yourAccount = new Account();
+
+        myAccount.name = "Bob";
+        myAccount.address = "222 Inner Lane"
+        myAccount.balance = 24.02;
+        yourAccount.name = "John";
+        yourAccount.address = "121 Outer Street"
+        yourAccount.balance = 55.63;
+
+
+        out.println(myAccount.name);
+        out.println("");
+        out.println(myAccount.address);
+        out.println();
+        out.println(myAccount.balance);
+        out.println();
+        out.println(yourAccount.name);
+        out.println("");
+        out.println(yourAccount.address);
+        out.println();
+        out.println(yourAccount.balance);
+        out.println();
+
+
+
+    }
+}
