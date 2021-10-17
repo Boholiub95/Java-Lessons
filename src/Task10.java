23c19d501180f60def4cee9b0feb3c80deb7c196
OlehBoholiub <boholiub95@gmail.com>
2021-10-17 06:00:09 -0600

    Task 9-10

---
+++ src/Task10.java
@@ -0,0 +1,14 @@
+public class Task10 {
+    public static void main(String[] args) {
+        switch (2) {
+            case 1:
+                System.out.println(1);
+            case 2:
+                System.out.println(2);
+            case 3:
+                System.out.println(3);
+            default:
+                System.out.println(0);
+        }
+    }
+}
