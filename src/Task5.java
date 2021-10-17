192e473e469e81c28126e666e5c34a73399e3800
OlehBoholiub <boholiub95@gmail.com>
2021-10-17 05:04:17 -0600

    Task5

---
+++ src/Task5.java
@@ -0,0 +1,9 @@
+public class Task5 {
+    public static void main(String[] args) {
+        String line1 = "123";
+        String line2 = new String("123");
+        System.out.println(line1 =="123");
+        System.out.println(line1 == line2);
+    }
+}
+/*truefalse*/
